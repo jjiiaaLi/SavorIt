6e354a2a19c1dbd0f533da16067c3b8570001d2f
Jack <fighterbee87@gmail.com>
2021-08-31 15:18:56 -0700

    logo

---
+++ react-app/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from '../image/Savor.png';
+import '../index.css'
+
+export default function Landing(){
+
+    return(
+        <>
+            <img className='logo' src={Logo} alt='logo'></img>
+        </>
+    )
+}
