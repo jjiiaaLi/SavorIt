f09c36aea0a1ea86ac8b283cddbad1cad8e065f4
Jack <fighterbee87@gmail.com>
2021-09-03 11:06:16 -0700

    meh progress

--- react-app/src/components/Landing.js
+++ react-app/src/components/Landing.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import LogoutButton from './auth/LogoutButton';
 import Logo from '../image/Savor.png';
 import '../index.css'
 
 export default function Landing(){
 
-    return(
-        <div className='phoneScreen'>
-            <img className='logo' src={Logo} alt='logo'></img>
+    return (
+      <>
+        <div className="landingTopBackground" />
+        <div className="landingTopContent">
+            <div></div>
+            
+            <p>Create An Account</p>
+            <p>Login</p>
         </div>
-    )
+      </>
+    );
 }
--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,34 +1,40 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import Logo from "../image/Savor.png";
 import '../index.css'
 
 const NavBar = () => {
   return (
     <nav>
       <div className="navContainer">
-        <div className="navItem">
-          <Link to="/" exact={true} className="navLink">
-            Home
-          </Link>
+        <div className='navLogoContainer'>
+          <img className="logo" src={Logo} />
         </div>
-        <div className="navItem">
-          <Link to="/login" exact={true} className="navLink">
-            Login
-          </Link>
-        </div>
-        <div className="navItem">
-          <Link to="/sign-up" exact={true} className="navLink">
-            Sign Up
-          </Link>
-        </div>
-        <div className="navItem">
-          <Link to="/users" exact={true} className="navLink">
-            Users
-          </Link>
-        </div>
-        <div className="navItem">
-          <LogoutButton />
+        <div className= 'navBtnContainer'>
+          <div className="navItem">
+            <Link to="/" exact={true} className="navLink">
+              Home
+            </Link>
+          </div>
+          <div className="navItem">
+            <Link to="/login" exact={true} className="navLink">
+              Login
+            </Link>
+          </div>
+          <div className="navItem">
+            <Link to="/sign-up" exact={true} className="navLink">
+              Sign Up
+            </Link>
+          </div>
+          <div className="navItem">
+            <Link to="/users" exact={true} className="navLink">
+              Users
+            </Link>
+          </div>
+          <div className="navItem">
+            <LogoutButton />
+          </div>
         </div>
       </div>
     </nav>
