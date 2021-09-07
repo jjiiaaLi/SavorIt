b6fa54608aff66f819cb185b5be0e6bb52902a38
Jack <fighterbee87@gmail.com>
2021-09-07 15:09:14 -0700

    upload image preview works but no idea how it works

--- react-app/src/components/AddPost.js
+++ react-app/src/components/AddPost.js
@@ -1,32 +1,38 @@
-import React,{useState, useEffect} from 'react';
+import React,{useState, useEffect, useRef} from 'react';
 import {useSelector} from 'react-redux';
 import {useHistory, useParams} from 'react-router-dom';
+import '../index.css'
 
 export default function AddPost(){
     const {userId} = useParams() // userId is a string here
     const [location, setLocation] = useState('');
     const [itemName, setItemName] = useState('');
     const [comment, setComment] = useState('');
-    const [image, setImage] = useState();
+    
+    const imageInput = useRef()
 
     const handleImageSelect = e =>{
-        const imageUploaded = e.target.files[0]
-        setImage(imageUploaded)
-        console.log(imageUploaded)
-
+        const file = e.target.files[0]
+        const reader = new FileReader()
+        const {current} = imageInput
+        current.file = file
+        reader.onload=(e)=>{
+            current.src = e.target.result
+        }
+        reader.readAsDataURL(file)
     }
 
     const submitPost = (e)=>{
         e.preventDefault()
-        console.log(image)
+        
     }
 
     return(
 
         <div>
             <form onSubmit={submitPost}>
-                {image && <img src={image}/>}
-                <input type='file' value={image} onChange={handleImageSelect}/>
+                <img className='imgPreview' ref={imageInput}/>
+                <input type='file'  onChange={handleImageSelect}/>
                 <input value={itemName} onChange={e=>{setItemName(e.target.value)}}/>
                 <input value={comment} onChange={e=>{setComment(e.target.value)}}/>
                 
