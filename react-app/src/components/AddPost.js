0c6f9779498c54eb4416d8f94c84f2b771cbabe8
Jack <fighterbee87@gmail.com>
2021-09-07 11:18:33 -0700

    post form

--- react-app/src/components/AddPost.js
+++ react-app/src/components/AddPost.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
+import {useSelector} from 'react-redux';
+import {useHistory, useParams} from 'react-router-dom';
 
 export default function AddPost(){
+    const userId = useParams()
+    
 
-
+    const submitPost = (e)=>{
+        e.preventDefault()
+        
+    }
 
     return(
 
-        <h1>add Post here</h1>
+        <div>
+            <form onSubmit={submitPost}>
+                <input>Location</input>
+                <input>itemName</input>
+                <input>comment</input>
+                <input>image</input>
+                <button type='submit'>submit</button>
+            </form>
+        </div>
     )
 }
